f94454188ecb620ee3980d73985e4b4bee0cd156
id774 <idnanashi@gmail.com>
2013-11-22 17:25:43 +0900

    7 Refactoring, improvement spec

--- lib/stdout.rb
+++ lib/stdout.rb
@@ -2,11 +2,11 @@
 # Name::      Stdout
 # Author::    774 <http://id774.net>
 # Created::   Nov 11, 2013
-# Updated::   Nov 12, 2013
+# Updated::   Nov 22, 2013
 # Copyright:: 774 <http://id774.net> Copyright (c) 2013
 # License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
 
 module Stdout
-  VERSION = "0.0.2"
   require File.dirname(__FILE__) +  "/stdout/output"
+  require File.dirname(__FILE__) +  "/stdout/version"
 end
--- spec/lib/stdout_spec.rb
+++ spec/lib/stdout_spec.rb
@@ -3,11 +3,9 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Stdout do
-  context 'const get :VERSION should' do
-    it "return right version number" do
-      expect = '0.0.2'
-      Stdout.const_get(:VERSION).should be_true
-      Stdout.const_get(:VERSION).should eq expect
-    end
+  context "VERSION" do
+    subject { Stdout::VERSION }
+
+    it { expect(subject).to eql "0.0.2" }
   end
 end
