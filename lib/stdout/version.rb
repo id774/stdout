4320bc43b758869d258b10f272195abae10d2dd2
id774 <idnanashi@gmail.com>
2013-12-13 18:05:33 +0900

    11 Version bump to v0.0.3

--- lib/stdout/version.rb
+++ lib/stdout/version.rb
@@ -1,10 +1,10 @@
 # Name::      Stdout::Version
 # Author::    774 <http://id774.net>
 # Created::   Nov 22, 2013
-# Updated::   Nov 22, 2013
+# Updated::   Dec 13, 2013
 # Copyright:: 774 Copyright (c) 2013
 # License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
 
 module Stdout
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
